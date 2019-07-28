a456632f62d4afa1476c7533517044e791ffc23c
mikan3rd <mikan.no.kikoushi@gmail.com>
2019-07-29 00:15:32 +0900

    feat: 24時間のコミットを参照するようにした

--- commitly/views.py
+++ commitly/views.py
@@ -1,10 +1,15 @@
 import re
+from datetime import datetime
 from pprint import pprint
 
+import dateutil.parser
+import pytz
 import requests
+from dateutil.relativedelta import relativedelta
 from django.http.response import JsonResponse
 from rest_framework.views import APIView
 
+tz = pytz.timezone("Asia/Tokyo")
 github_base_url = "https://api.github.com"
 
 
@@ -12,14 +17,20 @@ class Tweet(APIView):
     def get(self, request, format=None):
         username = "mikan3rd"
         url = f"{github_base_url}/users/{username}/events"
-        print(url)
         response = requests.get(url).json()
 
         result = {"others": 0}
+        now = datetime.now(tz)
+        print(now)
 
         for event in response:
 
             if not event["type"] == "PushEvent":
+                continue
+
+            created_at = dateutil.parser.parse(event["created_at"])
+
+            if now - relativedelta(days=1) > created_at:
                 continue
 
             for commit in event["payload"]["commits"]:
@@ -35,17 +46,16 @@ class Tweet(APIView):
 
                 for file_ in response.get("files", []):
                     search_result = re.search(r"\.\w+$", file_["filename"])
-                    additions = file_["additions"]
+                    changes = file_["changes"]
 
                     if not search_result:
-                        result["others"] += additions
+                        result["others"] += changes
                         continue
 
                     extension = search_result.group()
                     if not result.get(extension):
                         result[extension] = 0
 
-                    result[extension] += additions
+                    result[extension] += changes
 
-        pprint(result)
         return JsonResponse(result)
